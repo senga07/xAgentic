76a1d2fcfadb4454d11f985374166d329e507157
sjhuang05 <sjhuang05@mail.yst.com.cn>
2025-11-03 14:27:38 +0800

    [minor]remove supervisor

--- xAgentic-frontend/server.js
+++ xAgentic-frontend/server.js
@@ -148,7 +148,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`前端服务器运行在 http://localhost:${PORT}`);
-  console.log(`后端API地址: ${BACKEND_URL}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`前端服务器运行在 http://0.0.0.0:${PORT}`);
 });
